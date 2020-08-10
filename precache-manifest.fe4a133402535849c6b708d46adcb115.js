f25e272de0d920a009b19ed8f39564c1f2b77299
SakethK <sakethk98@gmail.com>
2020-08-09 23:01:56 -0700

    Updates

---
+++ precache-manifest.fe4a133402535849c6b708d46adcb115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d014b43a817b1cf573fd4dc25c71961",
+    "url": "/portf/index.html"
+  },
+  {
+    "revision": "e5715b20ef78b745cfe3",
+    "url": "/portf/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d88ba1f3775e10b94578",
+    "url": "/portf/static/js/2.195e9e7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portf/static/js/2.195e9e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5715b20ef78b745cfe3",
+    "url": "/portf/static/js/main.5b40ded9.chunk.js"
+  },
+  {
+    "revision": "0700ce093da7849dd878",
+    "url": "/portf/static/js/runtime-main.f89d973a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portf/static/media/logo.5d5d9eef.svg"
+  }
+]);
